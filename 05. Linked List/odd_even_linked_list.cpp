71c6c529b93ab1ab4428de01247b0b21a2c94e5f
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-27 00:40:24 +0530

    Linked List Questions

---
+++ 05. Linked List/odd_even_linked_list.cpp
@@ -0,0 +1,51 @@
+/*
+Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+*/
+
+#include<iostream>
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    
+    // Default constructor
+    ListNode() : val(0), next(nullptr) {}
+
+    // Constructor that initializes with a given value and null next pointer
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    // Constructor that initializes with a given value and a pointer to the next node
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode *odd = head, *even = head -> next;
+        ListNode *evenHead = head -> next;
+        while (even && even -> next) {
+            odd -> next = odd -> next -> next;
+            even -> next = even -> next -> next;
+            odd = odd -> next;
+            even = even -> next;
+        }
+        odd -> next = evenHead;
+        return head;
+    }
+};
