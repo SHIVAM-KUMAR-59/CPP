c3c1d7e49665e78b94658be85ca2c7badf37fb1f
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-28 20:02:15 +0530

    Greedy Algorithm Questions

--- 06. Greedy Algorithm/1. Easy/candy.cpp
+++ 06. Greedy Algorithm/1. Easy/candy.cpp
@@ -11,58 +11,126 @@ Return the minimum number of candies you need to have to distribute the candies 
 #include<iostream>
 using namespace std;
 
-class Solution {
-public:
+// SC: O(2n)
+// class Solution {
+// public:
+//     int candy(vector<int>& ratings) {
+//         int n = ratings.size();
+//         int leftNeighbour[n];
+//         leftNeighbour[0] = 1;
+//         // int rightNeighbour[n];
+//         // rightNeighbour[n - 1] = 1;
+        
+//         // for(int i = 1; i < n; i++){
+//         //     if(ratings[i] > ratings[i - 1]){
+//         //         leftNeighbour[i] = leftNeighbour[i - 1] + 1;
+//         //     }
+//         //     else{
+//         //         leftNeighbour[i] = 1;
+//         //     }
+//         // }
+
+//         // for(int i = n - 2; i >= 0; i--){
+//         //     if(ratings[i] > ratings[i + 1]){
+//         //         rightNeighbour[i] = rightNeighbour[i + 1] + 1;
+//         //     }
+//         //     else{
+//         //         rightNeighbour[i] = 1;
+//         //     }
+//         // }
+
+//         // int sum = 0;
+//         // for(int i = 0; i < n; i++){
+//         //     sum += max(leftNeighbour[i], rightNeighbour[i]);
+//         // }
+
+//         for(int i = 1; i < n; i++){
+//             if(ratings[i] > ratings[i - 1]){
+//                 leftNeighbour[i] = leftNeighbour[i - 1] + 1;
+//             }
+//             else{
+//                 leftNeighbour[i] = 1;
+//             }
+//         }
+
+//         int right = 1, current = 1, sum = max(1, leftNeighbour[n - 1]);
+//         for(int i = n - 2; i >= 0; i--){
+//             if(ratings[i] > ratings[i + 1]){
+//                 current += 1;
+//                 right = current;
+//             }else{
+//                 current = 1;
+//             }
+//             sum +=  max(leftNeighbour[i], current);
+//         }
+
+//         return sum;
+//     }
+// };
+
+// SC: O(n)
+// class Solution {
+// public:
+//     int candy(vector<int>& ratings) {
+//         int n = ratings.size();
+//         int leftNeighbour[n];
+//         leftNeighbour[0] = 1;
+    
+//         for(int i = 1; i < n; i++){
+//             if(ratings[i] > ratings[i - 1]){
+//                 leftNeighbour[i] = leftNeighbour[i - 1] + 1;
+//             }
+//             else{
+//                 leftNeighbour[i] = 1;
+//             }
+//         }
+
+//         int right = 1, current = 1, sum = max(1, leftNeighbour[n - 1]);
+//         for(int i = n - 2; i >= 0; i--){
+//             if(ratings[i] > ratings[i + 1]){
+//                 current += 1;
+//                 right = current;
+//             }else{
+//                 current = 1;
+//             }
+//             sum +=  max(leftNeighbour[i], current);
+//         }
+
+//         return sum;
+//     }
+// };
+
+// SC: O(1)
+// Slope based approach
+class Solution{
+    public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        int leftNeighbour[n];
-        leftNeighbour[0] = 1;
-        // int rightNeighbour[n];
-        // rightNeighbour[n - 1] = 1;
-        
-        // for(int i = 1; i < n; i++){
-        //     if(ratings[i] > ratings[i - 1]){
-        //         leftNeighbour[i] = leftNeighbour[i - 1] + 1;
-        //     }
-        //     else{
-        //         leftNeighbour[i] = 1;
-        //     }
-        // }
-
-        // for(int i = n - 2; i >= 0; i--){
-        //     if(ratings[i] > ratings[i + 1]){
-        //         rightNeighbour[i] = rightNeighbour[i + 1] + 1;
-        //     }
-        //     else{
-        //         rightNeighbour[i] = 1;
-        //     }
-        // }
-
-        // int sum = 0;
-        // for(int i = 0; i < n; i++){
-        //     sum += max(leftNeighbour[i], rightNeighbour[i]);
-        // }
-
-        for(int i = 1; i < n; i++){
-            if(ratings[i] > ratings[i - 1]){
-                leftNeighbour[i] = leftNeighbour[i - 1] + 1;
+        int sum = 1;
+        int i = 1;
+        while(i < n){
+            if(ratings[i] == ratings[i - 1]){
+                sum += 1;
+                i++;
+                continue;
             }
-            else{
-                leftNeighbour[i] = 1;
+            int peak = 1;
+            while(i < n && ratings[i] > ratings[i - 1]){
+                peak++;
+                sum += peak;
+                i++;
+            }
+            int down = 1;
+            while(i < n && ratings[i] < ratings[i - 1]){
+                sum += down;
+                i++;
+                down++;
+            }
+
+            if(down > peak){
+                sum += (down - peak);
             }
         }
-
-        int right = 1, current = 1, sum = max(1, leftNeighbour[n - 1]);
-        for(int i = n - 2; i >= 0; i--){
-            if(ratings[i] > ratings[i + 1]){
-                current += 1;
-                right = current;
-            }else{
-                current = 1;
-            }
-            sum +=  max(leftNeighbour[i], current);
-        }
-
         return sum;
     }
 };
